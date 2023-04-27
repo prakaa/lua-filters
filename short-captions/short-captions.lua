ee1d36362e3522ef1005f17174cfffa08099efe9
prakaa <abiprakash007@gmail.com>
2023-04-27 21:05:48 +1000

    Better code from jpcirrus

--- short-captions/short-captions.lua
+++ short-captions/short-captions.lua
@@ -1,25 +1,11 @@
-if FORMAT:match "latex" then
-  function figure_image (elem)
-    local fig_content = elem.content and elem.content[1]
-    if fig_content.t == "Plain" then
-      local image = fig_content.content and fig_content.content[1].t == "Image"
-        and fig_content.content[1]
-      return image
-    else
-      return nil
-    end
-  end
+PANDOC_VERSION:must_be_at_least '3.1'
 
-  function Figure (fig)
-    local img = figure_image(fig)
-    local img_attrs = img.attr.attributes
-    if not img or not img.caption or not img_attrs['short-caption'] then
-      return nil
+if FORMAT:match "latex" then
+  function Figure(f)
+    local short = f.content[1].content[1].attributes['short-caption']
+    if short and not f.caption.short then
+      f.caption.short = pandoc.Inlines(short)
     end
-    local short_caption = pandoc.Span(
-      pandoc.read(img_attrs['short-caption']).blocks[1].c
-    )
-    fig.caption.short = short_caption
-    return fig
+    return f
   end
 end
